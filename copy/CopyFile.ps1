8270bcb40fb23f0f6e573ec47eb3bf2f0d611a08
sunge <mrsunge@gmail.com>
2015-09-11 16:31:51 +0200

    modify comment. update next todo(step.txt)

--- copy/CopyFile.ps1
+++ copy/CopyFile.ps1
@@ -1,10 +1,14 @@
 ﻿param([string]$ConfFileName, [datetime]$date)
 
-
+##############################################
+#import module file.
 . C:\dba\script\copy\ParseConfFile.ps1
 . C:\dba\script\copy\MakeFileList.ps1
 
 
+
+##############################################
+#load information
 $conf = ParseConfFile $ConfFileName
 
 $SrcPath = $conf['SrcPath']
@@ -13,15 +17,20 @@ $DstPath = $conf['DstPath']
 $flist = MaekFileListFile $SrcPath $date
 
 
-echo "================================================" >> $flist
-echo "copy result" >> $flist
-echo "================================================" >> $flist
-echo "" >> $flist;
-
+##############################################
+# start copying.
 Get-Content $flist | ForEach-Object {
     if ($_.ToString().Length -gt 0) {
         $result = Robocopy.exe /r:2 $SrcPath $DstPath $_.ToString().Replace(' ', '')
     }
 }
+
+
+##############################################
+## save result
+echo "================================================" >> $flist
+echo "copy result" >> $flist
+echo "================================================" >> $flist
+echo "" >> $flist;
 
 $result >> $flist;
