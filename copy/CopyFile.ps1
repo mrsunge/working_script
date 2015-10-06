7b1e0856408cf57c893e71a11e051dc1fa0ef976
sunge <mrsunge@gmail.com>
2015-10-06 15:39:21 +0200

    conf.txt : add HOST kyeword. CopyFile.ps1 : create date folder to destination folder.

--- copy/CopyFile.ps1
+++ copy/CopyFile.ps1
@@ -1,4 +1,6 @@
-﻿param([string]$ConfFileName, [datetime]$date)
+﻿# This script is run on destination computer.
+
+param([string]$ConfFileName, [datetime]$date)
 
 ##############################################
 #import module file.
@@ -8,14 +10,29 @@
 
 
 ##############################################
-#load information
+# load information
 $conf = ParseConfFile $ConfFileName
 
 $SrcPath = $conf['SrcPath']
 $DstPath = $conf['DstPath']
+$DstPathDate = $DstPath + '/' + $(get-date).ToString('yyyyMMdd')
+
+
+##############################################
+# make destination folder
+if (-not (Test-Path $DstFullPath)) {
+    mkdir $DstFullPath
+}
 
 $flist = MaekFileListFile $SrcPath $date
 $fresult = $flist.Replace('.txt', '_result.txt');
+
+
+##############################################
+# login by net use
+$Hst = $conf['HOST']
+$Id = $conf['ID']
+$Pwd = $Conf['PW']
 
 
 ##############################################
@@ -23,6 +40,6 @@ $fresult = $flist.Replace('.txt', '_result.txt');
 echo "start copy" $date.ToString() > $fresult
 Get-Content $flist | ForEach-Object {
     if ($_.ToString().Length -gt 0) {
-        Robocopy.exe /r:2 $SrcPath $DstPath $_.ToString().Replace(' ', '') >> $fresult
+        Robocopy.exe /r:2 $SrcPath $DstPathDate $_.ToString().Replace(' ', '') >> $DstFullPath/$fresult
     }
 }
